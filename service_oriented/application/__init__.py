8990e9e6c9af2d1db61c6890a0cccb4ad1da4d95
Danny Guinther <dannyguinther@gmail.com>
2024-01-27 14:54:49 -0500

    Expose some classes through application package

---
+++ service_oriented/application/__init__.py
@@ -0,0 +1,8 @@
+from service_oriented.application.entry_point_spec import EntryPointSpec
+from service_oriented.application.generic_application import GenericApplication
+
+
+__all__ = [
+    "EntryPointSpec",
+    "GenericApplication",
+]
