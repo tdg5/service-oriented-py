103caa43dc38251ba7e403f4b68f1e17f92c83ba
Danny Guinther <dannyguinther@gmail.com>
2024-01-28 12:32:16 -0500

    Expose GenericMain through service_oriented.application

--- service_oriented/application/__init__.py
+++ service_oriented/application/__init__.py
@@ -1,8 +1,10 @@
 from service_oriented.application.base_application import BaseApplication
 from service_oriented.application.entry_point_spec import EntryPointSpec
+from service_oriented.application.generic_main import GenericMain
 
 
 __all__ = [
     "BaseApplication",
     "EntryPointSpec",
+    "GenericMain",
 ]
