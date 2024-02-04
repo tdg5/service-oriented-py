bf9b6cf26d6a3458e34442e0043d301e6e6c19e3
Danny Guinther <dannyguinther@gmail.com>
2024-02-04 11:01:34 -0500

    Fix initializer signature

--- service_oriented/logging/normalized_json_formatter.py
+++ service_oriented/logging/normalized_json_formatter.py
@@ -1,6 +1,6 @@
 import time
 from logging import LogRecord
-from typing import Dict
+from typing import Any, Dict
 
 from pythonjsonlogger.jsonlogger import JsonFormatter
 
@@ -8,12 +8,12 @@ from pythonjsonlogger.jsonlogger import JsonFormatter
 # Pretty much all of this is taken verbatim from
 # https://github.com/madzak/python-json-logger/blob/5f85723f4693c7289724fdcda84cfc0b62da74d4/README.md#customizing-fields
 class NormalizedJsonFormatter(JsonFormatter):
-    def __init__(self) -> None:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         # This init method exists to reduce the blast radius of type ignores
         # needed when instantiating this class. The GitHub issue to fix the
         # problem that requires this type ignore annotation can be found here:
         # https://github.com/madzak/python-json-logger/issues/173
-        super().__init__()  # type: ignore[no-untyped-call]
+        super().__init__(*args, **kwargs)  # type: ignore[no-untyped-call]
 
     def add_fields(
         self, log_record: Dict, record: LogRecord, message_dict: Dict
